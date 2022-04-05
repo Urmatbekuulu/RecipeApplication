c310b58733be06b76137783df0a4189cc52aecb7
Urmatbekuulu <raatbekurmatbekuulu@gmail.com>
2022-04-05 12:30:50 +0600

    Creating Recipe

---
+++ RecipeApplication/Pages/Recipes/Create.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RecipeApplication.Models;
+
+namespace RecipeApplication.Pages.Recipes
+{
+    public class CreateModel : PageModel
+    {
+        [BindProperty]
+        public CreateRecipeCommand Input { get; set; }
+
+        private readonly RecipeService _service;
+
+        public CreateModel(RecipeService service)
+        {
+            _service = service;
+        }
+        public void OnGet()
+        {
+            Input = new CreateRecipeCommand();
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var id = await _service.CreateRecipe(Input);
+                    throw new Exception();
+                    
+                }
+                
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError(string.Empty,
+                    "An error occured saving the recipe");
+            }
+
+            return Page();
+        }
+    }
+}
