9ec32de98b6b916e46ede7b1a64c6a999038fe6c
Urmatbekuulu <raatbekurmatbekuulu@gmail.com>
2022-04-05 12:36:06 +0600

    creating view chtml

---
+++ RecipeApplication/Pages/Recipes/View.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RecipeApplication.Pages.Recipes
+{
+    public class View : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
